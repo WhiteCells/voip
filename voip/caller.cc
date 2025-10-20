ac16cfa5f814475fc850ca419461c3015997c61f
zhuyang <810275110@qq.com>
2025-10-20 14:32:27 +0800

    caller.cc

--- voip/caller.cc
+++ voip/caller.cc
@@ -359,5 +359,5 @@ void voip::Caller::onCallMediaState(pj::OnCallMediaStateParam &prm)
     }
 
     // aud_med->startTransmit(*m_audio_media_recorder);
-    //    m_agent_robot_media_port->startTransmit(*m_audio_media_recorder);
+    m_agent_robot_media_port->startTransmit(*m_audio_media_recorder);
 }
